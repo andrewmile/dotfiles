cf41d945ecb366d333e0878a044cc6a28dd8e29b
Andrew Morgan <andrew.morgan@abilto.com>
2018-09-08 15:50:14 -0400

    Display full POSIX path as Finder window title

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ dotfiles="$HOME/.dotfiles"
 # link sublime user folder
 echo "linking sublime user folder"
 ln -s "$dotfiles/sublime" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+
+echo "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
