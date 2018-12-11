6e1efec66d127c690387d9ce6f850e4e0bd3b4cb
Andrew Morgan <andrew.morgan@abilto.com>
2018-12-11 11:12:06 -0500

    add npm packages to install.sh and install in home directory

--- install.sh
+++ install.sh
@@ -8,3 +8,18 @@ ln -s "$dotfiles/sublime" "$HOME/Library/Application Support/Sublime Text 3/Pack
 
 echo "Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# install npm packages in home directory
+echo "moving npm global install directory to home folder"
+mkdir "${HOME}/.npm-packages"
+ln -s "$dotfiles/npm/.npmrc" "${HOME}/.npmrc"
+
+echo "installing global npm packages"
+npm install -g eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
+npm install -g fx
+npm install -g gulp
+npm install -g @vue/cli
+npm install -g yarn
+
+# list globally installed packages
+npm -g list --depth=0
