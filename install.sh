3faa919722a9b050ae6ed9dd459dfe51af19bd7f
Andrew Morgan <andrewmile@gmail.com>
2020-01-31 15:56:46 -0600

    Add karabiner config to dotfiles

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ dotfiles="$HOME/.dotfiles"
 echo "linking sublime user folder"
 ln -s "$dotfiles/sublime" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
+# link karabiner config
+echo "linking karabiner config"
+ln -s "$dotfiles/karabiner/karabiner.edn" "$HOME/.config/karabiner.edn"
+
 echo "Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
