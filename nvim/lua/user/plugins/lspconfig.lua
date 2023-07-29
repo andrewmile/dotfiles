0a96791a904b838cb29a105d4d5188705d2d76b4
Andrew Morgan <andrew@tighten.co>
2023-07-29 17:27:41 -0400

    Add nvim formatting

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -32,12 +32,52 @@ require('lspconfig').jsonls.setup({
   }
 })
 
+-- null-ls
+require('null-ls').setup({
+  sources = {
+    require('null-ls').builtins.code_actions.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.code_actions.gitsigns,
+    require('null-ls').builtins.code_actions.proselint,
+    require('null-ls').builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.diagnostics.proselint,
+    require('null-ls').builtins.diagnostics.gitlint,
+    require('null-ls').builtins.diagnostics.luacheck.with({
+      extra_args = { '--config', vim.fn.stdpath('config') .. '/.luacheckrc' },
+    }),
+    -- require("null-ls").builtins.diagnostics.phpstan,
+    require('null-ls').builtins.diagnostics.solhint,
+    require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
+    require('null-ls').builtins.formatting.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.formatting.prettierd,
+    require('null-ls').builtins.formatting.phpcsfixer,
+    require('null-ls').builtins.formatting.jq,
+    require('null-ls').builtins.formatting.rustywind,
+    require('null-ls').builtins.formatting.stylua,
+  },
+})
+
+require('mason-null-ls').setup({ automatic_installation = true })
+
 vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+vim.api.nvim_create_user_command('Format', 'vim.lsp.buf.formatting_seq_sync()', {})
 
 vim.diagnostic.config({
   virtual_text = false,
